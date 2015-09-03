9b83bd47bb253b1841cabf9311dbdd7f97e73268
Vincent Jousse <vincent@jousse.org>
2015-09-03 10:00:00 +0200

    Test is_number

--- tests/test_alpa_to_number_in_xml.py
+++ tests/test_alpa_to_number_in_xml.py
@@ -1,6 +1,14 @@
 import pytest
+from voxolab.xml_alpha_to_numbers import is_number
 
 class TestAlphaToNumberInXml:
 
     def test_good_xml(self, full_xml_root):
         assert len(full_xml_root) == 3
+
+    def test_is_number(self):
+        assert is_number('trois')
+        assert is_number('vingt-trois')
+        assert not is_number('vingt-trois-test')
+        assert not is_number('trentetrois')
+        assert not is_number('trente trois')
